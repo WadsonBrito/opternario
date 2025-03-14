35067d1a8209a25d69225fc97904dfcce986db6d
WadsonBrito <wadsonmelo@gmail.com>
2025-03-14 19:40:18 +0000

    opternario

--- opternario.c
+++ opternario.c
@@ -1,1 +1,10 @@
+# include <stdio.h>
+int main(){
+    int idade;
 
+    printf("Informe sua idade: ");
+    scanf("%d", &idade);
+
+    idade >= 18 ? printf("Você é maior de idade\n") : printf("Você é menor de idade\n");
+    return 0;
+}
